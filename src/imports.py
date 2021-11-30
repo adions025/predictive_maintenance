9f87506d9f1cb58a50de670d6b058234316c17da
Adonis Gonzalez <adions025@gmail.com>
2021-11-30 10:48:55 +0100

    imports updated

--- src/imports.py
+++ src/imports.py
@@ -3,25 +3,28 @@ import numpy as np
 import os
 
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
 from sklearn.model_selection import train_test_split
 
 import keras
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, Dropout, LSTM, Activation
 from keras.optimizers import Adam, SGD, Adadelta, Adagrad
 
 import itertools
 
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 from tensorflow.python.client import device_lib
 
 # Configuración warnings
-# ------------------------------------------------------------------------------
 import warnings
 warnings.filterwarnings('once')
 warnings.filterwarnings("ignore", category=DeprecationWarning)
